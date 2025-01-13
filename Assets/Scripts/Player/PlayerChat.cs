cbb762adeb77af79a87377947ffa9c8fc0b59e47
Ruukaze <113981373+JoyAbrian@users.noreply.github.com>
2025-01-13 16:00:01 +0800

    chat bubble follow

--- Assets/Scripts/Player/PlayerChat.cs
+++ Assets/Scripts/Player/PlayerChat.cs
@@ -7,20 +7,39 @@ public class PlayerChat : NetworkBehaviour
     [SerializeField] private GameObject chatBubblePrefab;
     [SerializeField] private Vector3 offset;
 
+    private GameObject currentChatBubble;
+
     public void SpawnChatBubble(string message)
     {
         if (!IsOwner) return;
 
-        GameObject newChatBubble = Instantiate(chatBubblePrefab, transform.position + offset, Quaternion.identity);
-        newChatBubble.transform.SetParent(transform);
-        newChatBubble.transform.localPosition = offset;
+        if (currentChatBubble == null)
+        {
+            currentChatBubble = Instantiate(chatBubblePrefab, transform.position + offset, Quaternion.identity);
+            currentChatBubble.transform.SetParent(transform);
+        }
 
-        TMP_Text chatText = newChatBubble.GetComponentInChildren<TMP_Text>();
+        TMP_Text chatText = currentChatBubble.GetComponentInChildren<TMP_Text>();
         if (chatText != null)
         {
             chatText.text = message;
         }
+    }
 
-        Destroy(newChatBubble, 5f);
+    void Update()
+    {
+        if (currentChatBubble != null)
+        {
+            currentChatBubble.transform.position = transform.position + offset;
+        }
+        DestroyChatBubble();
+    }
+
+    public void DestroyChatBubble()
+    {
+        if (currentChatBubble != null)
+        {
+            Destroy(currentChatBubble, 5f);
+        }
     }
 }
